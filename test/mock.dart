02a08f41ab7db3f4b0f15ec92114096b988fcf86
Faris Nasution <nasution.faris@gmail.com>
2013-12-21 19:38:18 +0700

    Preparation for unittests.

---
+++ test/mock.dart
@@ -0,0 +1,7 @@
+part of dhilla.test;
+
+class MockHttpRequest extends Mock implements HttpRequest {}
+
+class MockHttpResponse extends Mock implements HttpResponse {}
+
+class MockWebSocket extends Mock implements WebSocket {}
---
+++ test/test.dart
@@ -0,0 +1,9 @@
+library dhilla.test;
+
+import 'dart:io';
+
+import 'package:unittest/unittest.dart';
+import 'package:unittest/mock.dart';
+import 'package:dhilla/dhilla.dart';
+
+part 'mock.dart';
