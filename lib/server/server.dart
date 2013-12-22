275e4f390871a176952b4326028b63eceba446db
Faris Nasution <nasution.faris@gmail.com>
2013-12-22 15:44:09 +0700

    Adding factory constructor to Server class.

--- lib/server/server.dart
+++ lib/server/server.dart
@@ -8,6 +8,9 @@ abstract class Server {
   StreamController _controller;
   Set _middlewares;
 
+  factory Server(address, int port, {int backlog: 0}) =>
+      new _Server(address, port, backlog);
+
   void use(Middleware callback);
   Future close();
   void _onListen();
