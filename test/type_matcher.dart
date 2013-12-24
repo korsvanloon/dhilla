4b9e99eaacdf4afd016f462379a554a079117d87
Faris Nasution <nasution.faris@gmail.com>
2013-12-24 12:29:24 +0700

    Adding isTransferables to type_matcher.dart.

--- test/type_matcher.dart
+++ test/type_matcher.dart
@@ -2,6 +2,7 @@ part of dhilla.test;
 
 const isHttpRequest = const _IsHttpRequest();
 const isRequest = const _IsRequest();
+const isTransferables = const _IsTransferables();
 
 class _IsHttpRequest extends TypeMatcher {
   const _IsHttpRequest() : super('HttpRequest');
@@ -11,4 +12,9 @@ class _IsHttpRequest extends TypeMatcher {
 class _IsRequest extends TypeMatcher {
   const _IsRequest() : super('Request');
   bool matches(item, Map matchState) => item is Request;
+}
+
+class _IsTransferables extends TypeMatcher {
+  const _IsTransferables() : super('Transferables');
+  bool matches(item, Map matchState) => item is Transferables;
 }
